8365095545b44d8438f467fb6652bd43bc687bc9
juan-rod <juan.j.rodriguez2@gmail.com>
2020-04-06 17:36:19 -0500

    removed code to be implemented in class

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 function openMobileNav() {
   // get nav-list element from HTML document
-  let navList = document.querySelector('.navigation ul')
   // get hamburger element from HTML document
-  let hamburger = document.querySelector('.hamburger')
   
   // toggle class to show nav mobile
-  navList.classList.toggle('show-mobile-nav')
   // toggle class to show hamburger close when nav open
-  hamburger.classList.toggle('hamburger-close')
 }
