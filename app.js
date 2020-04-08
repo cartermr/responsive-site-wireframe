b94ecb61d9e99aa33e3dd5c886f5c4ff4b82e72c
juan-rod <juan.j.rodriguez2@gmail.com>
2020-04-08 13:53:04 -0500

    added scroll function

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ function openMobileNav () {
   // toggle class to show hamburger close when nav open
   hamburger.classList.toggle('hamburger-close')
 }
+
+let adjustNavSizeOnScroll = () => {
+  let navbar = document.querySelector('nav.navigation')
+  if (window.scrollY > 100) {
+    navbar.classList.add('scroll-nav')
+  } else {
+    navbar.classList.remove('scroll-nav')
+  }
+}
+window.addEventListener('scroll', adjustNavSizeOnScroll)
+
+// create an eventlistener for scroll
+// in function, get navbar element
+// window scrollY
+// if scrollY > pixel, add class => scroll-nav
+// else remove scroll-nav
+// create scroll-nav class, change height of nav
+// adjust font size of .logo elements and list-items
